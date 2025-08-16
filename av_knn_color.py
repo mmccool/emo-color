1b406c0ec8aeb163a88b446fb4f4078c07c7713a
Michael McCool <michael.mccool@intel.com>
2025-08-16 11:35:22 +0000

    Update av_knn_color.py

--- av_knn_color.py
+++ av_knn_color.py
@@ -1,3 +1,4 @@
+# Map a color to the k closest emotions
 print(mapper.nearest_emotions_for_color("#ffd700", k=7))  # 'gold' in hex
 print(mapper.nearest_emotions_for_color((255, 64, 0), k=7))  # RGB tuple
 print(mapper.nearest_emotions_for_color("royalblue", k=7))   # CSS name
