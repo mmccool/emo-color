6adb5bd916188a181dca0b65f58d167ae1c896ed
Michael McCool <michael.mccool@intel.com>
2025-08-16 11:33:52 +0000

    Create av_knn_color.py

---
+++ av_knn_color.py
@@ -0,0 +1,3 @@
+print(mapper.nearest_emotions_for_color("#ffd700", k=7))  # 'gold' in hex
+print(mapper.nearest_emotions_for_color((255, 64, 0), k=7))  # RGB tuple
+print(mapper.nearest_emotions_for_color("royalblue", k=7))   # CSS name
