c7ae64b0f3e11382742b6562540d6e05c0ee384d
Michael McCool <michael.mccool@intel.com>
2025-08-16 11:33:19 +0000

    Create av_export.py

---
+++ av_export.py
@@ -0,0 +1,14 @@
+import json
+preds = mapper.predict_all()
+export = {
+    emo: {
+        "hex": cp.hex,
+        "rgb": cp.rgb,
+        "hsl": [float(cp.hsl[0]), float(cp.hsl[1]), float(cp.hsl[2])],
+        "nearest_css_name": cp.nearest_name
+    }
+    for emo, cp in preds.items()
+}
+with open("emotion_palette.json", "w", encoding="utf-8") as f:
+    json.dump(export, f, indent=2)
+print("Wrote emotion_palette.json")
