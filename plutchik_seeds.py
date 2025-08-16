97908ddcce41365a915e2238106588776f9be4d3
Michael McCool <michael.mccool@intel.com>
2025-08-16 11:58:11 +0000

    Create plutchik_seeds.py

---
+++ plutchik_seeds.py
@@ -0,0 +1,11 @@
+# Plutchik’s primary emotion color seeds (standard mapping)
+PLUTCHIK_COLOR_SEEDS = {
+    "joy":         "#FFFF00",  # Yellow
+    "trust":       "#00FF00",  # Green
+    "fear":        "#00FFFF",  # Cyan
+    "surprise":    "#0000FF",  # Blue
+    "sadness":     "#800080",  # Purple
+    "disgust":     "#008000",  # Dark Green
+    "anger":       "#FF0000",  # Red
+    "anticipation":"#FFA500",  # Orange
+}
